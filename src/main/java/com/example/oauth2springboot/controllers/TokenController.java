3f518ab776965e654abc5ff56b3f1c3b074bc95c
Vikram <vbedi@expediagroup.com>
2021-06-05 18:07:59 +0530

    basic setup

---
+++ src/main/java/com/example/oauth2springboot/controllers/TokenController.java
@@ -0,0 +1,50 @@
+package com.example.oauth2springboot.controllers;
+
+import com.example.oauth2springboot.models.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class TokenController {
+
+    @PostMapping("user")
+    public User login(@RequestParam("user") String username, @RequestParam("password") String password){
+
+        User user=new User();
+        user.setUser(username);
+
+        // Authenticate username/password in real project
+
+        String token=getJwtToken(username);
+        user.setToken(token);
+
+        return user;
+    }
+
+    private String getJwtToken(String username){
+        String secretKey="mySecretKey";
+        List<GrantedAuthority> grantedAuthorities= AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER");
+
+        String token= Jwts.builder()
+                .setId("myJWT")
+                .setSubject(username)
+                .claim("authorities",
+                        grantedAuthorities.stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+600000))
+                .signWith(SignatureAlgorithm.HS512,secretKey.getBytes()).compact();
+
+        return "Bearer "+token;
+    }
+
+}
